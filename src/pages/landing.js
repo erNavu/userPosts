116fd0aa826731ec35f083fd2a2e5ab1a274d87c
Navpreet <navpreet.boparai@prepladder.com>
2022-06-12 19:15:47 +0530

    initial commit

---
+++ src/pages/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserTable from "../components/userTable/userTable";
+
+const Landing = () => {
+  return (
+    <div>
+      <UserTable />
+    </div>
+  );
+};
+
+export default Landing;
